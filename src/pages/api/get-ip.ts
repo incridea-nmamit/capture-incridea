594f683094478706aee4589461e818c823737e65
Shishir Karkera <159174711+shishirkarkeraa@users.noreply.github.com>
2024-10-24 15:21:58 +0530

    Created get-ip.ts for webAnalytics

---
+++ src/pages/api/get-ip.ts
@@ -0,0 +1,7 @@
+// pages/api/get-ip.ts
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || '';
+  res.status(200).send(ip);
+}
