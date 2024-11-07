80173b5ca44357fb8b673008b9b682f45f6025c6
shishirkarkeraa <dn.shishirgk@gmail.com>
2024-11-07 20:04:23 +0530

    Update Title Description

--- src/components/TitleDescription.tsx
+++ src/components/TitleDescription.tsx
@@ -13,8 +13,8 @@ const TitleDescription: React.FC<TitleDescriptionProps> = ({ title, description,
       style={{ backgroundImage: `url('${imagePath}')` }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-65 flex flex-col items-center justify-center p-4">
-        <h1 className="text-4xl font-Hunters md:text-8xl text-white text-center">{title}</h1>
-        <p className="mt-2 text-base md:text-lg text-gray-300 max-w-2xl text-center">
+        <h1 className="text-6xl font-Hunters md:text-8xl text-white text-center">{title}</h1>
+        <p className="text-sm mt-2 md:text-lg text-gray-300 max-w-2xl text-center">
           {description}
         </p>
       </div>
