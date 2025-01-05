290d42a9ea077d9f6e0a6549a3ac9e7cb213733a
shishirkarkeraa <dn.shishirgk@gmail.com>
2025-01-05 23:00:09 +0530

    fix: mulitple like bug

--- src/components/CapturePopup.tsx
+++ src/components/CapturePopup.tsx
@@ -64,7 +64,6 @@ const CapturePopup: React.FC<CapturePopupProps> = ({
           userId: session_user,
         });
 
-        // Handle the returned status
         setHasLiked(userLikeStatus || false);
       } catch (error) {
         console.error("Error fetching initial data:", error);
@@ -85,14 +84,15 @@ const CapturePopup: React.FC<CapturePopupProps> = ({
           userId: sessionId,
           toggle: !hasLiked,
         });
-        setHasLiked(!hasLiked); // Optimistic update
+
+        // Optimistic update
+        setHasLiked(!hasLiked);
         setTotalLikes((prev) => (prev !== null ? prev + (hasLiked ? -1 : 1) : prev));
       } catch (error) {
         console.error("Error toggling like:", error);
       }
     }
   };
-
 
   if (!selectedImage) return null;
 
@@ -103,15 +103,15 @@ const CapturePopup: React.FC<CapturePopupProps> = ({
       aria-modal="true"
       onClick={handleClosePopup}
     >
-
-
-      <div className="max-w-[80vw] max-h-[90vh] w-full h-full grid grid-cols-1 md:grid-cols-2 bg-primary-900 gap-4 rounded-lg" >
+      <div className="max-w-[80vw] max-h-[90vh] w-full h-full grid grid-cols-1 md:grid-cols-2 bg-primary-900 gap-4 rounded-lg">
         <div className="bg-slate-800 h-full grid place-content-center rounded-lg overflow-hidden m-4">
-          <Image src={selectedImage || "/images/fallback.jpg"}
+          <Image
+            src={selectedImage || "/images/fallback.jpg"}
             alt="Selected"
             width={200}
             height={200}
-            layout="responsive" />
+            layout="responsive"
+          />
         </div>
 
         <div className="flex flex-col justify-between">
@@ -120,18 +120,18 @@ const CapturePopup: React.FC<CapturePopupProps> = ({
               <h4 className="font-Teknaf text-4xl text-center mb-6">Category name</h4>
               <div className="flex flex-row gap-2 items-center border-2 border-white rounded-full p-4">
                 <div className="rounded-full border-2 border-white overflow-hidden h-24 aspect-square">
-                  <Image src={selectedImage || "/images/fallback.jpg"}
+                  <Image
+                    src={selectedImage || "/images/fallback.jpg"}
                     className="w-full !h-full object-cover"
                     alt="Selected"
                     width={100}
                     height={100}
-                    layout="responsive" />
+                    layout="responsive"
+                  />
                 </div>
                 <div className="space-y-2 flex-grow flex flex-col text-center font-Trap-Regular">
                   <span className="text-sm text-gray-400">Captured by</span>
-                  <span className="text-lg">
-                    member name
-                  </span>
+                  <span className="text-lg">member name</span>
                   <span className="text-gray-400 text-sm">team name</span>
                 </div>
               </div>
@@ -139,25 +139,33 @@ const CapturePopup: React.FC<CapturePopupProps> = ({
           </div>
           <div>
             <div className="text-center py-5 m-4">
-              <p className="text-xs mx-auto max-w-lg mb-4 w-full"> Note: If you prefer this capture not to be public or have any
-                issues.We’ll verify your request and work on it soon.</p>
-              <button className="flex-grow bg-white rounded-2xl text-black p-2 px-5 hover:scale-105 transition-all" onClick={() => {
-                handleClosePopup();
-                openRemovalPopup(selectedImage);
-              }}>
+              <p className="text-xs mx-auto max-w-lg mb-4 w-full">
+                Note: If you prefer this capture not to be public or have any issues. We’ll verify your request and work
+                on it soon.
+              </p>
+              <button
+                className="flex-grow bg-white rounded-2xl text-black p-2 px-5 hover:scale-105 transition-all"
+                onClick={() => {
+                  handleClosePopup();
+                  openRemovalPopup(selectedImage);
+                }}
+              >
                 Request Removal
               </button>
             </div>
 
             <div className="flex gap-4 w-full rounded-lg p-5">
-            <button onClick={handleToggleLike} aria-label="Like Button">
-              <FaHeart size={30} color={hasLiked ? "red" : "white"} />
-            </button>
-            <span className="flex items-center">{totalLikes !== null ? totalLikes : "..."}</span>
+              <button onClick={handleToggleLike} aria-label="Like Button">
+                <FaHeart size={30} color={hasLiked ? "red" : "white"} />
+              </button>
+              <span className="flex items-center">{totalLikes !== null ? totalLikes : "..."}</span>
               <button>
                 <Share2 />
               </button>
-              <button className="flex-grow bg-white rounded-2xl text-black mx-10 p-2 px-6 hover:scale-[101%] transition-all" onClick={() => handleDownload(selectedImageOg || selectedImage)}>
+              <button
+                className="flex-grow bg-white rounded-2xl text-black mx-10 p-2 px-6 hover:scale-[101%] transition-all"
+                onClick={() => handleDownload(selectedImageOg || selectedImage)}
+              >
                 Download
               </button>
             </div>
--- src/server/api/routers/like.ts
+++ src/server/api/routers/like.ts
@@ -1,14 +1,7 @@
-import {  createTRPCRouter, protectedProcedure, publicProcedure} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc";
 import { z } from "zod";
 
 export const likeRouter = createTRPCRouter({
-  // Get all events
-  getLike: publicProcedure.query(async ({ ctx }) => {
-    const like = await ctx.db.galleryLike.findMany({
-    });
-    return like ?? [];
-  }),
-
   getTotalLikes: protectedProcedure
     .input(
       z.object({
@@ -16,98 +9,39 @@ export const likeRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ ctx, input }) => {
-        const totalLikes = await ctx.db.galleryLike.count({
-            where: { galleryId: input.captureId, liked: true },
-          });     
-
+      const totalLikes = await ctx.db.galleryLike.count({
+        where: { galleryId: input.captureId, liked: true },
+      });
       return totalLikes;
     }),
 
-  // Add a new event
-  like: protectedProcedure
-  .input(
-    z.object({
-      galleryId: z.number().min(1, "Id is required"),
-    })
-  )
-  .mutation(async ({ ctx, input }) => {
-    const userId = ctx.session?.user?.id;
-
-    if (!userId) {
-      alert("User not authenticated");
-    }
-
-    const newLike = await ctx.db.galleryLike.create({
-      data: {
-        galleryId: input.galleryId,
-        userId,
-      },
-    });
-
-    return newLike;
-  }),
-
-
-    likeUsers: protectedProcedure
+  hasLiked: protectedProcedure
     .input(
       z.object({
-        galleryId: z.number().min(1, "Id is required"),
+        galleryId: z.number(),
+        userId: z.string(),
       })
     )
     .mutation(async ({ ctx, input }) => {
-        const users = await ctx.db.galleryLike.findMany({
-            where: { galleryId: input.galleryId, liked: true },
-            include: { user: true },
-          });
-                
+      const likeStatus = await ctx.db.galleryLike.findUnique({
+        where: { userId_galleryId: { userId: input.userId, galleryId: input.galleryId } },
+      });
+      return likeStatus?.liked || false;
     }),
 
-
-    hasLiked: protectedProcedure
+  toggleLike: protectedProcedure
     .input(
       z.object({
-        galleryId: z.number().min(1, "Id is required"),
-        userId: z.string().min(1, "userId is required")
+        galleryId: z.number(),
+        userId: z.string(),
+        toggle: z.boolean(),
       })
     )
     .mutation(async ({ ctx, input }) => {
-        const hasLiked = await ctx.db.galleryLike.findUnique({
-            where: { userId_galleryId: { userId: input.userId, galleryId: input.galleryId } },
-          });
-          return hasLiked?.liked;          
+      await ctx.db.galleryLike.upsert({
+        where: { userId_galleryId: { userId: input.userId, galleryId: input.galleryId } },
+        update: { liked: input.toggle },
+        create: { userId: input.userId, galleryId: input.galleryId, liked: input.toggle },
+      });
     }),
-
-    toggleLike: protectedProcedure
-  .input(
-    z.object({
-      galleryId: z.number().min(1, "Id is required"),
-      userId: z.string().min(1, "userId is required"),
-      toggle: z.boolean(),
-    })
-  )
-  .mutation(async ({ ctx, input }) => {
-    const { galleryId, userId, toggle } = input;
-
-    // Check if the user exists in the User table
-    const userExists = await ctx.db.user.findUnique({ where: { id: userId } });
-
-    if (!userExists) {
-      throw new Error(`User with ID ${userId} does not exist.`);
-    }
-
-    // Check if the gallery exists in the Gallery table
-    const galleryExists = await ctx.db.gallery.findUnique({ where: { id: galleryId } });
-
-    if (!galleryExists) {
-      throw new Error(`Gallery with ID ${galleryId} does not exist.`);
-    }
-
-    // If both user and gallery exist, perform the upsert operation
-    await ctx.db.galleryLike.upsert({
-      where: { userId_galleryId: { userId, galleryId } },
-      update: { liked: toggle },
-      create: { userId, galleryId, liked: toggle },  // Default liked to the toggle value
-    });
-  }),
-
 });
