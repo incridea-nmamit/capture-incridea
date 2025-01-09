0f705bb2cd3922bd07460f0f83e1613b7361eec4
Shishir Karkera <159174711+shishirkarkeraa@users.noreply.github.com>
2025-01-09 08:53:08 +0530

    Update index.tsx

--- src/pages/our-team/socialmedia/index.tsx
+++ src/pages/our-team/socialmedia/index.tsx
@@ -72,6 +72,12 @@ const SocialMediaPage: React.FC = () => {
               name={member.name}
               designation={member.designation}
               say={member.say}
+stats={[
+        {
+          title: "Experience",
+          bigText: "5 years",
+          regularText: "In software development",
+        }]}
             />
         ))}
       </div>
