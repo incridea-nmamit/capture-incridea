fa6532ed1c0ca44a1d90cdfa18ecd54b7e5a82a5
Nandan Pai <nandanrp09@gmail.com>
2025-01-22 15:39:01 +0530

    fix: landing parallax background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
-import { HomeUi } from "~/components/homePage/page";
-
+import Images from "~/components/Landing/Images";
+import { ParallaxHero } from "~/components/Landing/Parallax";
+import TimeLine from "~/components/Landing/TimeLine";
 
 export default function Home() {
   return (
     <>
-      <HomeUi/>
+      <ParallaxHero />
+      <Images />
+      <TimeLine />
     </>
   );
 }
