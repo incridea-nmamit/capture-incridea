f4fb6ddb601395698771627eef39587540e65859
VarshithPawarHR <varshithpawarhr@gmail.com>
2025-01-31 10:51:47 +0530

    fixed mobile nav

--- src/pages/our-team/developers/index.tsx
+++ src/pages/our-team/developers/index.tsx
@@ -1,17 +1,25 @@
-import { api } from '~/utils/api';
-import TeamCard from '~/components/TeamPage/TeamCard'; // Adjust path if needed
-import TitleSection from '~/components/TeamPage/TeamTitle';
-import FallingClipart from '~/components/BackgroundFallAnimation/FallingClipart';
-import CameraLoading from '~/components/LoadingAnimation/CameraLoading';
+import { api } from "~/utils/api";
+import TeamCard from "~/components/TeamPage/TeamCard"; // Adjust path if needed
+import TitleSection from "~/components/TeamPage/TeamTitle";
+
+import CameraLoading from "~/components/LoadingAnimation/CameraLoading";
 
 const MediaCommittee: React.FC = () => {
-  const { data: teamMembers, isLoading, error } = api.team.getAllTeams.useQuery(); // Fetch data with tRPC
+  const {
+    data: teamMembers,
+    isLoading,
+    error,
+  } = api.team.getAllTeams.useQuery(); // Fetch data with tRPC
 
   if (isLoading) {
-    return <CameraLoading/>;
+    return <CameraLoading />;
   }
   if (error) {
-    return <div className="text-red-500">Error loading media teams: {error.message}</div>;
+    return (
+      <div className="text-red-500">
+        Error loading media teams: {error.message}
+      </div>
+    );
   }
   if (!teamMembers || teamMembers.length === 0) {
     return <div className="text-white">No developers found.</div>;
@@ -19,50 +27,47 @@ const MediaCommittee: React.FC = () => {
 
   // Sort team members by designation priority
   const sortedTeamMembers = teamMembers
-  .filter(member => member.committee === 'developer')
-  .sort((a, b) => {
-    const aDesignation = a.designation.toLowerCase();
-    const bDesignation = b.designation.toLowerCase();
+    .filter((member) => member.committee === "developer")
+    .sort((a, b) => {
+      const aDesignation = a.designation.toLowerCase();
+      const bDesignation = b.designation.toLowerCase();
 
+      const designationPriority = (designation: string) => {
+        if (designation.includes("lead")) {
+          return 1;
+        }
+        if (designation.includes("full")) {
+          return 2;
+        }
+        if (designation.includes("front")) {
+          return 3;
+        }
+        if (designation.includes("back")) {
+          return 4;
+        }
+        return 5;
+      };
 
-    const designationPriority = (designation: string) => {
-      if (designation.includes('lead')) {
-        return 1; 
+      const priorityA = designationPriority(aDesignation);
+      const priorityB = designationPriority(bDesignation);
+
+      if (priorityA === priorityB) {
+        return a.id - b.id;
       }
-      if (designation.includes('full')) {
-        return 2; 
-      }
-      if (designation.includes('front')) {
-        return 3; 
-      }
-      if (designation.includes('back')) {
-        return 4; 
-      }
-      return 5; 
-    };
 
-    const priorityA = designationPriority(aDesignation);
-    const priorityB = designationPriority(bDesignation);
-
-
-    if (priorityA === priorityB) {
-      return a.id - b.id; 
-    }
-
-    return priorityA - priorityB;
-  });
+      return priorityA - priorityB;
+    });
 
   return (
-    <div className="flex flex-col items-center gradient-bg z-20  ">
-      <FallingClipart />
+    <div className="gradient-bg z-20 flex flex-col items-center">
       {/* Title Section */}
-      <TitleSection 
+      <TitleSection
         title="Capture Incridea Developers"
         description="Developing for your convinience"
         backgroundImage="https://utfs.io/f/0yks13NtToBiNX5DnCjzD2wWm5AylYHcVfipk40e8O9RubFS"
       />
       {/* Cards Section */}
-      <div className="py-6 md:py-12 container-size flex flex-col md:flex-row flex-wrap justify-center gap-6 md:gap-8">
+      <div className="container-size flex flex-col flex-wrap justify-center gap-6 py-6 md:flex-row md:gap-8 md:py-12">
         {sortedTeamMembers.map((member, index) => (
           <TeamCard
             key={index}
