c2a47871301b18f3caf9d6320eb6daef44964f71
VarshithPawarHR <varshithpawarhr@gmail.com>
2025-01-31 10:40:42 +0530

    minor changes dated 29/01/2025

--- src/components/LoadingAnimation/CameraLoading.tsx
+++ src/components/LoadingAnimation/CameraLoading.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import styles from "./loader.module.css";
 
-
 const CenteredLoader = () => {
   return (
-    <div className="z-50 fixed inset-0 flex items-center justify-center bg-neutral-900">
+    <div className={styles.fullscreen}>
       <div className={styles.loader}>
         <div className={styles.box}>
           {/* <div className={styles.logo}>
--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,19 +1,19 @@
-import { Lobster, Sora, Space_Grotesk } from "next/font/google";
+import { Lobster, Space_Grotesk } from "next/font/google";
 
 const grotesk = Space_Grotesk({
-    weight: ["700"],
-    subsets: ["latin"],
-    variable: "--font-grotesk",
+  weight: ["700"],
+  subsets: ["latin"],
+  variable: "--font-grotesk",
 });
 
 const lobster = Lobster({
-    weight: ["400"],
-    subsets: ["latin"],
-    variable: "--font-lobster",
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-lobster",
 });
 const fonts = {
-    lobster,
-    grotesk,
+  lobster,
+  grotesk,
 };
 
 export { fonts };
